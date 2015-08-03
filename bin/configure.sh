a479675cc3a87f337cc70379e3bde0a19cc8131e
Luis Capelo <luiscape@gmail.com>
2015-08-03 15:26:00 -0400

    fixing db name

--- bin/configure.sh
+++ bin/configure.sh
@@ -4,4 +4,4 @@
 # Creates an user in the
 # MongoDB instance.
 #
-mongo --eval "db.createUser({user: '$MONGODB_USER_NAME', pwd: '$MONGODB_USER_PASSWORD', roles: [{ role: 'readWrite', db: '$MONGODB_DATABASE' },]})"
+mongo $MONGODB_DATABASE --eval "db.createUser({user: '$MONGODB_USER_NAME', pwd: '$MONGODB_USER_PASSWORD', roles: [{ role: 'readWrite', db: '$MONGODB_DATABASE' },]})"
