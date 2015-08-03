d0a1c7f95fba9764e78c55d41f55f3005636e226
Luis Capelo <luiscape@gmail.com>
2015-08-03 15:14:45 -0400

    changing path to bash

--- bin/configure.sh
+++ bin/configure.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 #
 # Creates an user in the
 # MongoDB instance.
 #
-mongo --eval \
-  "db.createUser({user: '$MONGODB_USER_NAME', pwd: '$MONGODB_USER_PASSWORD', roles: [{ role: 'readWrite', db: '$MONGODB_DATABASE' },]})"
+mongo --eval "db.createUser({user: '$MONGODB_USER_NAME', pwd: '$MONGODB_USER_PASSWORD', roles: [{ role: 'readWrite', db: '$MONGODB_DATABASE' },]})"
